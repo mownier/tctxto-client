04637c54ed61466591ee0326a48f00cbf14818af
Mounir Ybanez <rinuom91@gmail.com>
2025-04-04 21:20:27 +0800

    Added constants.

---
+++ frontend/src/constants/element-ids.ts
@@ -0,0 +1,4 @@
+export const MAIN_ROOT_ID = 'main-root'
+export const MAIN_ROOT_HEADER_ID = 'main-root-header'
+export const MAIN_ROOT_FOOTER_ID = 'main-root-footer'
+export const MAIN_ROOT_CONTENT_ID = 'main-root-content'
