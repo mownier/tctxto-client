6736975225c8b67fdf7e5b5f458969dfca1d441a
Mounir Ybanez <rinuom91@gmail.com>
2025-04-05 00:53:26 +0800

    Integrated createLobby gRPC request.

--- frontend/src/flows/show-create-lobby-view.ts
+++ frontend/src/flows/show-create-lobby-view.ts
@@ -1,10 +1,15 @@
 import { CreateLobbyView } from '../views/CreateLobbyView'
 import { showLobbyView } from './show-lobby-view'
 import * as ElementIds from "../constants/element-ids"
+import { createLobby } from '../grpc/client'
 
 export function showCreateLobbyView(): CreateLobbyView {
-    return new CreateLobbyView(ElementIds.MAIN_ROOT_CONTENT_ID, (playerName: string) => {
-        const lobbyId = "1"
-        showLobbyView(lobbyId)
+    return new CreateLobbyView(ElementIds.MAIN_ROOT_CONTENT_ID, async (playerName: string) => {
+        try {
+            const lobbyId = await createLobby(playerName)
+            showLobbyView(lobbyId)
+        } catch (error) {
+            console.log("An error occurred:", error)
+        }
     })
 }
