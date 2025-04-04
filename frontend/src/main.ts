67f69eca846391a8efe3d88608caba0347564418
Mounir Ybanez <rinuom91@gmail.com>
2025-04-04 21:23:20 +0800

    Used constants for the element ids.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,32 +1,32 @@
 import { CreateLobbyView } from './views/CreateLobbyView'
 import { LobbyView } from './views/LobbyView'
 import { GameView } from './views/GameView'
-import { setLocale, setLocalesDirectory } from './localization/localization'
+import { setLocale } from './localization/localization'
+import { MainRootView } from './views/MainRootView'
+import * as ElementIds from './constants/element-ids'
 
 async function main() {
-    setLocalesDirectory('/static/localization/locales/')
     await setLocale("fil")
+    new MainRootView(ElementIds.MAIN_ROOT_ID)
     showCreateLobbyView()
 }
 
 function showCreateLobbyView(): CreateLobbyView {
-    return new CreateLobbyView("main-root", (playerName: string) => {
+    return new CreateLobbyView(ElementIds.MAIN_ROOT_CONTENT_ID, (playerName: string) => {
         const lobbyId = "1"
         showLobbyView(lobbyId)
     })
 }
 
 function showLobbyView(lobbyId: string): LobbyView {
-    return new LobbyView("main-root", lobbyId, (player1: string, player2: string) => {
+    return new LobbyView(ElementIds.MAIN_ROOT_CONTENT_ID, lobbyId, (player1: string, player2: string) => {
         const gameId = "1"
         showGameView(gameId)
     })
 }
 
 function showGameView(gameId: string): GameView {
-    return new GameView("main-root", gameId)
+    return new GameView(ElementIds.MAIN_ROOT_CONTENT_ID, gameId)
 }
-
-
 
 main()
