f699f441443e3311b65bd513b5ce57c60900b0ef
Mounir Ybanez <rinuom91@gmail.com>
2025-04-11 13:23:54 +0800

    Refactored MainRootView.

--- frontend/src/views/MainRootView.ts
+++ frontend/src/views/MainRootView.ts
@@ -1,23 +1,24 @@
 import * as ElementIds from "../constants/element-ids"
 
 export class MainRootView {
-    constructor(containerId: string) {
-        const container = document.getElementById(containerId) as HTMLElement
-        
-        if (!container) {
-            throw new Error(`container with id ${containerId} not found`)
-        }
+    private rootElement: HTMLElement
 
+    constructor(rootElement: HTMLElement) {
+        this.rootElement = rootElement
+        this.initializeView()
+    }
+
+    private initializeView() {
         const header = document.createElement('div')
         header.id = ElementIds.MAIN_ROOT_HEADER_ID
-        container.appendChild(header)
+        this.rootElement.appendChild(header)
 
         const content = document.createElement('div')
         content.id = ElementIds.MAIN_ROOT_CONTENT_ID
-        container.appendChild(content)
+        this.rootElement.appendChild(content)
 
         const footer = document.createElement('div')
         footer.id = ElementIds.MAIN_ROOT_FOOTER_ID
-        container.appendChild(footer)
+        this.rootElement.appendChild(footer)
     }
 }
