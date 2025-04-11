9c19d527696cf64a0a63060e899a7cc85ea43a85
Mounir Ybanez <rinuom91@gmail.com>
2025-04-11 11:31:35 +0800

    Deleted flows.

--- frontend/src/flows/show-welcome-view.ts
+++
@@ -1,8 +0,0 @@
-import { WelcomeView } from "../views/WelcomeView"
-import * as ElementIds from "../constants/element-ids"
-import { showCreateLobbyView } from "./show-create-lobby-view"
-import { showJoinLobbyView } from "./show-join-lobby-view"
-
-export function showWelcomeView(): WelcomeView {
-    return new WelcomeView(ElementIds.MAIN_ROOT_CONTENT_ID, showCreateLobbyView, showJoinLobbyView)
-}
