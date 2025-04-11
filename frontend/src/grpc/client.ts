99471a890be34ea45a44c338449c48e9dc78f7dc
Mounir Ybanez <rinuom91@gmail.com>
2025-04-11 14:11:01 +0800

    Added exchange implementation.

--- frontend/src/grpc/client.ts
+++ frontend/src/grpc/client.ts
@@ -1,7 +1,42 @@
 import { TicTacToeClient } from "./TctxtoServiceClientPb"
 import * as GrpcConst from "../constants/grpc"
+import { ExchangeRequest } from "./tctxto_pb"
 
-function newClient(): TicTacToeClient {
+const CLIENT_ID_STORAGE_KEY = 'TicTacToeClient_clientId'
+
+function createClient(): TicTacToeClient {
     return new TicTacToeClient(`${GrpcConst.GRPC_WEB_PROXY_HOST}:${GrpcConst.GRPC_WEB_PROXY_PORT}`)
 }
 
+function storeClientId(clientId: string): void {
+    sessionStorage.setItem(CLIENT_ID_STORAGE_KEY, clientId)
+}
+
+function getClientId(): string {
+    return sessionStorage.getItem(CLIENT_ID_STORAGE_KEY) || ''
+}
+
+export async function exchange(): Promise<void> {
+    if (getClientId().length > 0) {
+        return
+    }
+
+    const client = createClient()
+    const request = new ExchangeRequest()
+    request.setPublicKey(GrpcConst.GRPC_SERVER_PK)
+
+    let clientId: string
+
+    try {
+        const reply = await client.exchange(request)
+        clientId = reply.getClientId()
+        if (!clientId) {
+            throw new Error("client ID is empty in exchange reply.")
+        }
+    } catch (error) {
+        console.error("errror during client ID exchange:", error)
+        throw error
+    }
+
+    storeClientId(clientId)
+}
