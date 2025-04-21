aa185f21641aac0dd18255ae6758a603811afc7c
Mounir Ybanez <rinuom91@gmail.com>
2025-04-21 17:22:41 +0800

    Added checking on empty TCTXTO_SERVER_PK and TCTXTO_PROXY_ORIGIN.

--- main.go
+++ main.go
@@ -6,20 +6,16 @@ import (
 	"log"
 	"net"
 	"os"
+	"strings"
 	"tctxtocl/server"
 )
 
-var (
-	port = "2323"
-	//go:embed templates/* static/*
-	fs embed.FS
-)
+//go:embed templates/* static/*
+var fs embed.FS
 
 func main() {
-	envPort := os.Getenv("TCTXTO_CLIENT_PORT")
-	if envPort != "" {
-		port = envPort
-	}
+	localIP := ""
+
 	addrs, err := net.InterfaceAddrs()
 	if err != nil {
 		log.Fatalf("error getting network interfaces: %v\n", err)
@@ -27,11 +23,34 @@ func main() {
 	for _, addr := range addrs {
 		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
 			if ipnet.IP.To4() != nil {
-				localIP := ipnet.IP.String()
-				log.Printf("tctxto client running on http://%s:%s\n", localIP, port)
+				localIP = ipnet.IP.String()
+				break
 			}
 		}
 	}
+
+	if len(localIP) == 0 {
+		log.Fatalln("unable to determine local IP")
+	}
+
+	emptyEnvVars := []string{}
+	if len(os.Getenv("TCTXTO_PROXY_ORIGIN")) == 0 {
+		emptyEnvVars = append(emptyEnvVars, fmt.Sprintf("set environment variable TCTXTO_PROXY_ORIGIN (e.g. export TCTXTO_PROXY_ORIGIN=http://%s:2121)", localIP))
+	}
+	if len(os.Getenv("TCTXTO_SERVER_PK")) == 0 {
+		emptyEnvVars = append(emptyEnvVars, "set environment variable TCTXTO_SERVER_PK (e.g. export TCTXTO_SERVER_PK=<consumer_public_key>)")
+	}
+	if len(emptyEnvVars) > 0 {
+		log.Fatalf("encountered problem(s): %s\n", strings.Join(emptyEnvVars, ", "))
+	}
+
+	port := os.Getenv("TCTXTO_CLIENT_PORT")
+	if len(port) == 0 {
+		port = "2323"
+	}
+
+	log.Printf("tctxto client running on http://%s:%s\n", localIP, port)
+
 	if err := server.ListenAndServe(fmt.Sprintf(":%s", port), fs); err != nil {
 		log.Fatalf("failed to listen and serve: %v\n", err)
 	}
