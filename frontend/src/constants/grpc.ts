dbc4157f6d1a6f967f95ffda8678e8262aac49d7
Mounir Ybanez <rinuom91@gmail.com>
2025-04-21 13:18:51 +0800

    Used environment variables.

--- frontend/src/constants/grpc.ts
+++ frontend/src/constants/grpc.ts
@@ -1,4 +1,2 @@
-export const GRPC_WEB_PROXY_HOST = 'http://localhost'
-export const GRPC_WEB_PROXY_PORT = 2121
-
-export const GRPC_SERVER_PK = '12345'
+export const GRPC_WEB_PROXY_ORIGIN: string = process.env.TCTXTO_PROXY_ORIGIN || ''
+export const GRPC_SERVER_PK: string = process.env.TCTXTO_SERVER_PK || ''
