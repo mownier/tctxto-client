87a0e0dfe634d9dfecc32dd220c32083ed5eb2e2
Mounir Ybanez <rinuom91@gmail.com>
2025-04-22 15:37:18 +0800

    Added page data.

--- server/server.go
+++ server/server.go
@@ -9,28 +9,33 @@ import (
 type server struct {
 }
 
-func newServer(mux *http.ServeMux, fs embed.FS) *server {
+func newServer(mux *http.ServeMux, d PageData, fs embed.FS) *server {
 	s := &server{}
 	mux.Handle("/static/", http.FileServer(http.FS(fs)))
-	mux.HandleFunc("/", s.indexHandler(fs))
+	mux.HandleFunc("/", s.indexHandler(d, fs))
 	return s
 }
 
-func (s *server) indexHandler(fs embed.FS) http.HandlerFunc {
+func (s *server) indexHandler(d PageData, fs embed.FS) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		t := template.Must(template.ParseFS(fs, "templates/index.html"))
-		err := t.Execute(w, nil)
+		err := t.Execute(w, d)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	}
 }
 
-func ListenAndServe(addr string, fs embed.FS) error {
+func ListenAndServe(addr string, d PageData, fs embed.FS) error {
 	m := http.NewServeMux()
-	newServer(m, fs)
+	newServer(m, d, fs)
 	if err := http.ListenAndServe(addr, m); err != nil {
 		return err
 	}
 	return nil
+}
+
+type PageData struct {
+	PublicKey   string
+	ProxyOrigin string
 }
